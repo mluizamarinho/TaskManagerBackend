cabb752ffe671e2b8d8ce776abd47ce7b8c11aac
mluizamarinho <maria.marinho@ccc.ufcg.edu.br>
2024-07-25 22:15:19 -0300

    rotas basicas do crud

--- server.js
+++ server.js
@@ -5,7 +5,28 @@ const path = require('path');
 
 app.get('/', (req, res) =>
 {
-    res.send('Hello World!');
+    res.send('Tarefa lida');
+})
+
+// rota para criar uma tarefa
+app.post('/create-task', (req, res) => {
+  
+  res.send('Tarefa criada')
+
+})
+
+// rota para deletar uma tarefa
+app.delete('/delete-task', (req, res) => {
+  
+  res.send('Tarefa deletada')
+
+})
+
+// rota para atualizar tarefa
+app.put('/update-task', (req, res) => {
+  
+  res.send('Tarefa atualizada')
+
 })
 
 app.listen(port, () => {
