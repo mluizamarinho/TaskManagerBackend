f1709e981c2847c69b4faad83be7ad52f4d8ed72
Maria <maria.marinho@ccc.ufcg.edu.br>
2024-08-18 14:59:26 -0300

    add repository de user

---
+++ src/infrastructure/repository/UserRepository.ts
@@ -0,0 +1,41 @@
+import PrismaConnection from '../database/connection';
+import { User, Prisma } from '@prisma/client';
+
+class UserRepository {
+    private prisma = PrismaConnection.getClient();
+
+    public async findAllUsers(): Promise<User[]> {
+        return this.prisma.user.findMany();
+    }
+
+    public async findUserById(id: string): Promise<User | null> {
+        return this.prisma.user.findUnique({
+            where: { id },
+            include: {
+                tasks: true,
+                boards: true
+            }
+        });
+    }
+
+    public async createUser(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({
+            data,
+        });
+    }
+
+    public async updateUser(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+        return this.prisma.user.update({
+            where: { id },
+            data,
+        });
+    }
+
+    public async deleteUser(id: string): Promise<User> {
+        return this.prisma.user.delete({
+            where: { id },
+        });
+    }
+}
+
+export default UserRepository;
