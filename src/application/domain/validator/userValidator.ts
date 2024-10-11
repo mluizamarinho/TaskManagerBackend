8f33af9a8eca297e378efa721eba5a43c1c6bce6
Maria <maria.marinho@ccc.ufcg.edu.br>
2024-10-11 17:50:30 -0300

    ultimas mud back

--- src/application/domain/validator/userValidator.ts
+++
@@ -1,8 +0,0 @@
-
-export function validateUserInput(name: string, password: string): void {
-    if (!name || !password) {
-      throw new Error('Nome de usuário e senha são obrigatórios');
-    }
-    
-  }
-  
